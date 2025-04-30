be984727e07480f649843647b52fde827d867750
Seth456879 <hattus.lima@nave.org.br>
2025-04-30 07:51:41 -0300

    create functions for calculate

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+
+function calcularQuantidadeArvores(litros) {
+    const litrosPorArvore = 100; // alterar para o numero real
+    return litros / litrosPorArvore;
+}
+
 function banho(quantidade, duracao) {
     const duracaoTotal = quantidade * duracao;
     const litrosPorMinuto = 9;
 
     return litrosPorMinuto * duracaoTotal;
+}
+
+function descarga(quantidade) {
+    const litrosPorMinuto = 12; // alterar para o valor real
+    return quantidade * litrosPorMinuto;
 }
